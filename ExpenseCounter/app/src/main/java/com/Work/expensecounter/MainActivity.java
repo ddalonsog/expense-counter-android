17e8f9f7f79590191562f5f1d068a6903731b732
ddalonsog <ddalonsog2004@gmail.com>
2026-01-18 22:40:33 -0500

    Implemented Add_Expense, Pop up message, Button and total_Expense

--- ExpenseCounter/app/src/main/java/com/Work/expensecounter/MainActivity.java
+++ ExpenseCounter/app/src/main/java/com/Work/expensecounter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.Work.expensecounter;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,58 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Variable para guardar el total (fuera de los métodos para que persista)
+    private double totalExpenses = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Configuración de los márgenes del sistema (Edge-to-Edge)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+       //Linking the xml file to the code using the IDs
+        EditText etAmount = findViewById(R.id.editTextAmount);
+        EditText etDescription = findViewById(R.id.editTextDescription);
+        Button btnAdd = findViewById(R.id.buttonAdd);
+        TextView tvTotal = findViewById(R.id.textViewTotal);
+
+        //Actions and behaivior for the botton
+        btnAdd.setOnClickListener(v -> {
+
+            String amountText = etAmount.getText().toString();
+
+
+            if (!amountText.isEmpty()) {
+
+                double amount = Double.parseDouble(amountText);
+                
+
+                totalExpenses += amount;
+
+
+                tvTotal.setText(String.format(Locale.getDefault(), "$%.2f", totalExpenses));
+
+
+                etAmount.setText("");
+                etDescription.setText("");
+
+
+                Toast.makeText(this, "Gasto agregado", Toast.LENGTH_SHORT).show();
+            } else {
+                // Si está vacío, mostramos un error
+                Toast.makeText(this, "Escribe un monto por favor", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
